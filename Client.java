1a43c89d73342b809a37e777803307b6ef620a47
Jeremy <jeremyludiaz@gmail.com>
2024-11-08 14:35:22 -0500

    Client to Server Communication Success. I was able to establish communication from Client.java to Server.java.

--- Client.java
+++ Client.java
@@ -1,3 +1,32 @@
+import java.io.*;
+import java.net.*;
+
+
 public class Client {
+
+    public static void main(String[] args) throws IOException{
+        Socket socket = new Socket("localHost", 4999);
+        
+        PrintWriter pr = new PrintWriter(socket.getOutputStream());
+        pr.println("Hello World");
+        pr.flush(); //forces any buffered output to be written to output stream
+
+    }
+
+    public void handshake(){
+
+    }
     
+    public void disconnect(){
+
+    }
+
+    public void request(){
+
+    }
+
+    public void getSocket(){
+
+    }
+
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,37 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket server = new ServerSocket(4999);
+        Socket socket = server.accept();
+
+        System.out.println("Client Connected");
+        
+        InputStreamReader in = new InputStreamReader(socket.getInputStream());
+        BufferedReader reader = new BufferedReader(in);
+
+        String string = reader.readLine();
+        System.out.println("Client: " + string);
+
+
+    }
     
+    public Server(){
+        
+    }
+
+    public void getConnectedTimes(){
+        /*
+            The server should record the time each client 
+            was connected so that it can properly return 
+            these values
+        */
+    }
+
+    public void disconnect(){
+        
+    }
+
 }
