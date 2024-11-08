a0fd0dc438616b8e0161ee3dd3719430598e4406
Jeremy <jeremyludiaz@gmail.com>
2024-11-08 17:35:26 -0500

    Implemented the fucntionalities into methods

--- Client.java
+++ Client.java
@@ -1,20 +1,31 @@
 import java.io.*;
+import java.lang.annotation.IncompleteAnnotationException;
 import java.net.*;
 
 
-public class Client {
+public class Client{
 
-    public static void main(String[] args) throws IOException{
-        Socket socket = new Socket("localHost", 4999);
-        
-        PrintWriter pr = new PrintWriter(socket.getOutputStream());
-        pr.println("Hello World");
-        pr.flush(); //forces any buffered output to be written to output stream
+    private String hostname = new String();
+    private int port;
+    private Socket socket;
 
+    public Client(String hostname, int port){
+        this.hostname = hostname;
+        this.port = port;
     }
 
     public void handshake(){
+        try{
+            socket = new Socket(hostname, port);
+            System.out.println("Connection Established");
 
+            PrintWriter pr = new PrintWriter(socket.getOutputStream());
+            pr.println("Hello World");
+            pr.flush();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
     
     public void disconnect(){
@@ -22,11 +33,19 @@ public class Client {
     }
 
     public void request(){
-
+        
     }
 
-    public void getSocket(){
+    public Socket getSocket(){
+        return this.socket;
+    }
+    
+    public int getPort(){
+        return this.port;
+    }
 
+    public String getLocalAddress(){
+        return this.hostname;
     }
 
 }
--- Server.java
+++ Server.java
@@ -1,11 +1,17 @@
 import java.io.*;
 import java.net.*;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class Server {
 
+    private ServerSocket serverSocket = new ServerSocket();
+    private int port;
+
+    private ArrayList<LocalDateTime> timeList = new ArrayList<>();
+
     public static void main(String[] args) throws IOException{
-        ServerSocket server = new ServerSocket(4999);
-        Socket socket = server.accept();
+        
 
         System.out.println("Client Connected");
         
@@ -18,8 +24,30 @@ public class Server {
 
     }
     
-    public Server(){
-        
+    public Server(int port){
+        this.port = port;
+    }
+
+    public serve(int serves){
+        try {
+            //CREATE A SERVERSOCKET WITH PORT
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server is reaching connection with Port" + port);
+
+            for (int i = 0; i < serves; i++){
+                //ACCEPT CONNECTION
+                Socket socket = serverSocket.accept();
+                System.out.println("New connection established");
+
+                //GET CURRENT TIMESTAMP AND SAVE TO ARRAYLIST
+                getConnectedTimes();
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error Serving Client: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public void getConnectedTimes(){
@@ -28,10 +56,20 @@ public class Server {
             was connected so that it can properly return 
             these values
         */
+        LocalDateTime now = LocalDateTime.now();
+        timeList.add(now);
+        System.out.println("New Connection Established at: " + now);
+
     }
 
     public void disconnect(){
-        
+        try {
+            serverSocket.close();
+            System.out.println("Server Successfully Closed");
+        } catch (IOException e) {
+            System.out.println("Error disconnecting from server: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 }
